31c9c8184433ba0b2d0c36f9f126299ca8104f0b
Sethu-madhav <sethu4784@gmail.com>
2022-11-19 00:34:06 +0530

    Added DbContext and Db migrations
    Added DbContext and Db migrations

---
+++ NotesApi/Data/NotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApi.Models.Entities;
+
+namespace NotesApi.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
